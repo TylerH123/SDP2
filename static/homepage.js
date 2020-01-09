33cefa7dfc1353133ac8ba65b7420c3c6b2dec2b
Tyler Huang <thuang00@stuy.edu>
2020-01-08 23:28:03 -0500

    push

--- static/homepage.js
+++ static/homepage.js
@@ -1,9 +1,9 @@
-var button = document.getElementById("roll");
-button.addEventListener('click', rand);
-
 var rand = function(){
   var num1 = Math.floor(Math.random() * 5 + 1);
   var num2 = Math.floor(Math.random() * 5 + 1);
-  ele.innerHTML = fib(len);
-  console.log(num1 + " " + num2);
+  var ele = document.getElementById("nums");
+  ele.innerHTML = num1 + " " + num2;
 }
+
+var button = document.getElementById("roll");
+button.addEventListener('click', rand);
