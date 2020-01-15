fe5f74440a8ef0f50bac6602a1472a70a7e56b2a
Tyler Huang <thuang00@stuy.edu>
2020-01-15 10:25:38 -0500

    push

--- reset.py
+++ reset.py
@@ -8,13 +8,7 @@ c = db.cursor()               #facilitate db ops
 command = "DROP TABLE IF EXISTS users;" # delete table
 c.execute(command)
 
-command = "DROP TABLE IF EXISTS info;" # delete table
-c.execute(command)
-
 command = "CREATE TABLE IF NOT EXISTS users (id INTEGER, username TEXT, password TEXT, coins INTEGER, streak INTEGER, timeStmp TEXT, farmLvl INTEGER);" # create table
-c.execute(command)    # run SQL statement
-
-command = "CREATE TABLE IF NOT EXISTS info (gameName TEXT, description TEXT, howTo TEXT, image TEXT);" # create table
 c.execute(command)    # run SQL statement
 
 db.commit()
